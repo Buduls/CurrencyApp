006ee9ade513adf77863c13c5fafb7de6a1f8066
Povilas Vytautas Cibas <Povilas.Vytautas.Cibas@visma.com>
2016-08-22 21:36:03 +0300

    Fetching currency differences fixed when full path is specified

--- CurrencyApp/CurrencyApp/Scripts/Custom/currencyApp.js
+++ CurrencyApp/CurrencyApp/Scripts/Custom/currencyApp.js
@@ -4,7 +4,7 @@ currencyApp.controller('currencyController', function ($scope, $http) {
     $scope.hideExchangeRates = true;
     $scope.GetExchangeRates = function () {
         if ($scope.exchangeRatesForm.$valid) {
-            $http.get("Home/GetExchangeRateDifferences", { params: { date: $scope.date } }).then(function (response) {
+            $http.get("GetExchangeRateDifferences", { params: { date: $scope.date } }).then(function (response) {
                 $scope.result = response.data;
                 $scope.hideExchangeRates = false;
             });
